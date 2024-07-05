a3946843db1cfd7e47d083f96cbd91b6cae35a71
Hagen Stanek <hagen@stanek.de>
2024-07-05 20:35:04 +0200

    Change Simple class to contain byte code corner cases

--- sample/src/sample/Simple.java
+++ sample/src/sample/Simple.java
@@ -5,15 +5,19 @@ package sample;
 public class  Simple
 {
 
-    public static int  calculate ()
-    {
-//      return 6*7;
-
-        int  MAX = 10;
-
-//        return MAX;
-        return Fibunacci. straight (MAX);
-//        return Fibunacci. recursive (MAX);
-    }
+	public static int  calculate ()
+	{
+		return 0;
+//		return 1;
+//		return 2;
+//		return 3;
+//		return 4;
+//		return 5;
+//		return 6;
+//		return 6*7;
+//		int  i = 6;  return i+7;
+//		return Fibunacci.straight(9);
+//		return Fibunacci.recursive(9);
+	}
 
 }
